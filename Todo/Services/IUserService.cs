aca7d9059a6715f87f81b176c2e737549d9299f0
maharjansujal <np01cp4a220124@islingtoncollege.edu.np>
2024-12-09 19:20:45 +0545

    first commit

---
+++ Todo/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Models;
+
+namespace Todo.Services
+{
+    public interface IUserService
+    {
+        Task SaveUserAsync(User user);
+        //implemented class (UserService):
+        //json file create if not alredy created
+        //if created AddUser.razor(we are trying to inject the service here) bata aako data list append json file update
+        //read user --> already created json file ko eata tanne
+        Task<List<User>> LoadUsersAsync();
+    }
+}
